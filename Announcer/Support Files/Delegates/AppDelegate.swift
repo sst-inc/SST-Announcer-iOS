b8688e0ceaa995f6faa016f28244219a60059cb6
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-05-22 22:29:33 +0800

    replaced deprecated code got notifications working again

--- Announcer/Support Files/Delegates/AppDelegate.swift
+++ Announcer/Support Files/Delegates/AppDelegate.swift
@@ -9,25 +9,22 @@
 import UIKit
 import UserNotifications
 import BackgroundTasks
+import SafariServices
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
-    var window: UIWindow?
-    
+    static let backgroundTaskIdentifier = Bundle.main.bundleIdentifier! + ".new-announcement"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Checks to ensure the URL is correct.
+        // Safeguard against my 3am stupidity
         #if DEBUG
         #else
         if blogURL != "http://studentsblog.sst.edu.sg" {
             fatalError("incorrect URL")
         }
         #endif
-        
-        // Fetch data every five minutes.
-        // This deprecated thing is for the next exco to deal with.
-        UIApplication.shared.setMinimumBackgroundFetchInterval(300)
         
         //Ask for notification authorization
         let center = UNUserNotificationCenter.current()
@@ -36,118 +33,53 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             (granted, error) in
             if !granted {
                 print("Something went wrong")
+                print("noooo i demand notifications!!!!!")
             }
         }
+        
+        // Notification delegate
+        UNUserNotificationCenter.current().delegate = self
+        
+        // Performing Background Fetch for new posts
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: AppDelegate.backgroundTaskIdentifier, using: .main) { task in
+            
+            // Check if there is a new post
+            if let notificationContent = fetchNotificationsTitle() {
+                // Notify the user if there is a new post
+                self.pushNotification(with: notificationContent.title, content: notificationContent.content)
+            }
+            
+            task.setTaskCompleted(success: true)
+            
+            // Handle running out of time
+            task.expirationHandler = {
+                print("uh oh")
+            }
+            
+            // Schedule the next background task
+            self.scheduleBackgroundTaskIfNeeded()
+        }
+        
+        // Start background task
+        scheduleBackgroundTaskIfNeeded()
         
         return true
     }
     
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let posts = fetchValues()
+    // Testing if background task works
+    // Run this below in debugger
+    // e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"sg.edu.sst.panziyue.Announcer.new-announcement"]
+    func scheduleBackgroundTaskIfNeeded() {
+        let taskRequest = BGProcessingTaskRequest(identifier: AppDelegate.backgroundTaskIdentifier)
+        taskRequest.requiresNetworkConnectivity = true
         
-        var post: Post?
-        
-        for item in posts {
-            if item.title == userInfo["title"] as! String {
-                post = item
-                break
-            }
+        do {
+            try BGTaskScheduler.shared.submit(taskRequest)
+        } catch {
+            print("Unable to schedule background task: \(error)")
         }
-        
-        if let announcementVC = window?.rootViewController as? AnnouncementsViewController, let post = post {
-            
-            announcementVC.receivePost(with: post)
-        }
-    }
-    
-    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        
-        if let notificationContent = fetchNotificationsTitle() {
-            let notifications: [UNMutableNotificationContent] = { () -> [UNMutableNotificationContent] in
-                let content = UNMutableNotificationContent()
-                content.title = notificationContent.title
-                content.body = notificationContent.content
-                content.sound = .default
-                content.userInfo = ["title" : notificationContent.title]
-                
-                return [content]
-            }()
-            
-            for notification in notifications {
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-                let request = UNNotificationRequest(identifier: "NewPost", content: notification, trigger: trigger)
-                let center = UNUserNotificationCenter.current()
-                center.add(request, withCompletionHandler: nil)
-            }
-        }
-        
-    }
-    
-    override func restoreUserActivityState(_ activity: NSUserActivity) {
-        fatalError()
-    }
-    
-    func application(_ application: UIApplication, willContinueUserActivityWithType userActivityType: String) -> Bool {
-        
-        print("oh wow")
-        
-        let posts = fetchValues()
-        
-        let info = (userActivity!.userInfo as? [String:String])!
-        
-        var post: Post?
-        
-        for item in posts {
-            if item.title == info["title"] && item.content == info["content"] {
-                
-                post = item
-                break
-            }
-        }
-        
-        if let announcementVC = window?.rootViewController as? AnnouncementsViewController, let post = post {
-            
-            announcementVC.receivePost(with: post)
-            
-            return true
-        }
-
-        
-        return false
-    }
-    
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-        
-        print("does this work")
-        
-        let posts = fetchValues()
-        
-        let info = (userActivity.userInfo as? [String:String])!
-        
-        var post: Post?
-        
-        for item in posts {
-            if item.title == info["title"] && item.content == info["content"] {
-                
-                post = item
-                break
-            }
-        }
-        
-        if let announcementVC = window?.rootViewController as? AnnouncementsViewController, let post = post {
-            
-            announcementVC.receivePost(with: post)
-            
-            return true
-        }
-
-        
-        return false
-    }
-    
-    func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
-        print(error.localizedDescription)
-        print("!!")
+        // To test if background tasks work, add a breakpoint here
+        // Then run the code above in the debugger
     }
     
     // MARK: UISceneSession Lifecycle
@@ -167,5 +99,92 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
+    // New Push Notifications thing
+    func pushNotification(with postTitle: String, content postContent: String) {
+        let identifier = "new-announcement"
+        let content = UNMutableNotificationContent()
+        
+        content.title = "📢 \(postTitle)"
+        content.body = postContent
+        
+        content.sound = .default
+        
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Error showing error user notification: \(error)")
+                }
+            }
+        }
+    }
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        // Calls when user opens app from a notification
+        
+        openPost(with: response.notification)
+        
+        completionHandler()
+    }
+    
+    func openPost(with notification: UNNotification) {
+        let posts = fetchValues()
+        
+        var post: Post?
+        
+        let notificationContent = notification.request.content
+        
+        var postTitle = notificationContent.title
+        postTitle.removeFirst(2)
+        
+        // Finding the post to present
+        for item in posts {
+            if item.title == postTitle {
+                
+                post = item
+                
+                break
+            }
+        }
+        
+        let navigationController = UIApplication.shared.windows.first?.rootViewController as! UINavigationController
+        let announcementVC = navigationController.topViewController as! AnnouncementsViewController
+        
+        if let post = post {
+            // Handles when post is found
+            
+            announcementVC.receivePost(with: post)
+            
+        } else {
+            // Handle when unable to get post
+            // Show an alert to the user to tell them that the post was unable to be found :(
+
+            print("failed to get post :(")
+
+            let alert = UIAlertController(title: "Unable to get post", message: "Something went wrong when trying to retrieve the post. You can try to open this post in Safari.", preferredStyle: .alert)
+
+            // If user opens post in Safari, it will simply bring them to student blog home page
+            alert.addAction(UIAlertAction(title: "Open in Safari", style: .default, handler: { (_) in
+                let svc = SFSafariViewController(url: URL(string: blogURL)!)
+                
+                announcementVC.present(svc, animated: true)
+            }))
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            
+            announcementVC.present(alert, animated: true)
+            
+        }
+    }
 }
