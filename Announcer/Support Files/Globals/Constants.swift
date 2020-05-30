218b3ca15324a2aac754ab0533be053db1bdfede
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-05-31 01:11:27 +0800

    cleaned up several constants

--- Announcer/Support Files/Globals/Constants.swift
+++ Announcer/Support Files/Globals/Constants.swift
@@ -27,7 +27,7 @@ struct GlobalLinks {
      
      This constant stores the URL for the blog linked to the RSS feed.
      */
-    static let blogURL = "http://studentsblog.sst.edu.sg"
+    static let blogURL             = "http://studentsblog.sst.edu.sg"
     
     /**
      URL for the blogURL's RSS feed
@@ -36,14 +36,113 @@ struct GlobalLinks {
      
      This URL is the blogURL but with the path of the RSS feed added to the back.
      */
-    static let rssURL = URL(string: "\(GlobalLinks.blogURL)/feeds/posts/default")!
+    static let rssURL              = URL(string: "\(GlobalLinks.blogURL)/feeds/posts/default")!
 
     /**
      Error 404 website
      
      This URL is to redirect users in a case of an error while getting the blog posts or while attempting to show the student's blog.
      */
-    static let errorNotFoundURL = URL(string: "https://sstinc.org/404")!
+    static let errorNotFoundURL    = URL(string: "https://sstinc.org/404")!
+    
+    /**
+     Gets the share URL from a `Post`
+     
+     - returns: The URL of the blog post
+     
+     - parameters:
+        - post: The post to be shared
+      
+     - important: This method handles error 404 by simply returning the `blogURL`
+     
+     - note: Versions of SST Announcer before 11.0 shared the full content of the post instead of the URL
+     
+     This method generates a URL for the post by merging the date and the post title.
+    */
+    static func getShareURL(with post: Post) -> URL {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "/yyyy/MM/"
+        
+        var shareLink = ""
+        
+        let formatted = post.title.filter { (a) -> Bool in
+            a.isLetter || a.isNumber || a.isWhitespace
+        }.lowercased()
+        let split = formatted.split(separator: " ")
+        
+        for i in split {
+            if shareLink.count + i.count < 40 {
+                shareLink += i + "-"
+            } else {
+                break
+            }
+        }
+        shareLink.removeLast()
+        
+        // 40 chars
+        shareLink = GlobalLinks.blogURL + dateFormatter.string(from: post.date) + shareLink + ".html"
+        
+        let shareURL = URL(string: shareLink) ?? URL(string: GlobalLinks.blogURL)!
+        
+        // Checking if the URL brings up a 404 page
+        let isURLValid: Bool = {
+            let str = try? String(contentsOf: shareURL)
+            if let str = str {
+                return !str.contains("Sorry, the page you were looking for in this blog does not exist.")
+            } else {
+                return false
+            }
+        }()
+        
+        if isURLValid {
+            return shareURL
+        }
+        
+        return URL(string: GlobalLinks.blogURL)!
+    }
+
+    /**
+     Gets the links within the `Post`
+     
+     - returns: An array of `URL`s which are in the post.
+     
+     - parameters:
+        - post: The selected post
+     
+     - important: This process takes a bit so it is better to do it asyncronously so the app will not freeze while searching for URLs.
+     
+     This method gets the URLs found within the blog post and filters out images from blogger's content delivery network because no one wants those URLs.
+    */
+    static func getLinksFromPost(post: Post) -> [URL] {
+        let items = post.content.components(separatedBy: "href=\"")
+        
+        var links: [URL] = []
+        
+        for item in items {
+            var newItem = ""
+            
+            for character in item {
+                if character != "\"" {
+                    newItem += String(character)
+                } else {
+                    break
+                }
+            }
+            
+            if let url = URL(string: newItem) {
+                links.append(url)
+            }
+        }
+        
+        links.removeDuplicates()
+        
+        links = links.filter { (link) -> Bool in
+            !link.absoluteString.contains("bp.blogspot.com/")
+        }
+        
+        return links
+    }
+
 }
 
 /**
@@ -56,19 +155,19 @@ struct GlobalColors {
     static let blueTint = UIColor.systemBlue
     
     /// Border Color for Scroll Selection
-    static let borderColor = GlobalColors.blueTint.withAlphaComponent(0.3).cgColor
+    static let borderColor         = GlobalColors.blueTint.withAlphaComponent(0.3).cgColor
     
     /// Background color for App
-    static let background = UIColor(named: "Background")!
+    static let background          = UIColor(named: "Background")!
     
     /// First Grey Color
-    static let greyOne = UIColor(named: "Grey 1")!
+    static let greyOne             = UIColor(named: "Grey 1")!
     
     /// Second Grey Color
-    static let greyTwo = UIColor(named: "Grey 2")!
+    static let greyTwo             = UIColor(named: "Grey 2")!
     
     /// Global Tint
-    static let globalTint = UIColor(named: "Global Tint")!
+    static let globalTint          = UIColor(named: "Global Tint")!
 }
 
 /**
@@ -78,19 +177,19 @@ struct GlobalColors {
  */
 struct GlobalIdentifier {
     /// Cell identifier for labels in filterViewController and contentViewController
-    static let labelCell = "labels"
+    static let labelCell           = "labels"
     
     /// Cell identifier for announcements in announcementViewController
-    static let announcementCell = "announcements"
+    static let announcementCell    = "announcements"
     
     /// Cell identifier for links in contentViewController
-    static let linkCell = "links"
+    static let linkCell            = "links"
     
     /// Background Task Identifier
-    static let backgroundTask = Bundle.main.bundleIdentifier! + ".new-announcement"
+    static let backgroundTask      = Bundle.main.bundleIdentifier! + ".new-announcement"
     
     /// New announcement notification identifier
-    static let newNotification = "new-announcement"
+    static let newNotification     = "new-announcement"
 }
 
 /**
--- Announcer/Support Files/Globals/Functions.swift
+++ Announcer/Support Files/Globals/Functions.swift
@@ -96,9 +96,7 @@ func fetchBlogPosts(_ vc: AnnouncementsViewController) -> [Post] {
             }))
             
             // Just dismiss it
-            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { action in
-                
-            }))
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
             
             vc.present(alert, animated: true, completion: nil)
         }
@@ -219,104 +217,6 @@ func getLabelsFromSearch(with query: String) -> String {
 }
 
 /**
- Gets the share URL from a `Post`
- 
- - returns: The URL of the blog post
- 
- - parameters:
-    - post: The post to be shared
-  
- - important: This method handles error 404 by simply returning the `blogURL`
- 
- - note: Versions of SST Announcer before 11.0 shared the full content of the post instead of the URL
- 
- This method generates a URL for the post by merging the date and the post title.
-*/
-func getShareURL(with post: Post) -> URL {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "/yyyy/MM/"
-    
-    var shareLink = ""
-    
-    let formatted = post.title.filter { (a) -> Bool in
-        a.isLetter || a.isNumber || a.isWhitespace
-    }.lowercased()
-    let split = formatted.split(separator: " ")
-    
-    for i in split {
-        if shareLink.count + i.count < 40 {
-            shareLink += i + "-"
-        } else {
-            break
-        }
-    }
-    shareLink.removeLast()
-    
-    // 40 chars
-    shareLink = GlobalLinks.blogURL + dateFormatter.string(from: post.date) + shareLink + ".html"
-    
-    let shareURL = URL(string: shareLink) ?? URL(string: GlobalLinks.blogURL)!
-    
-    // Checking if the URL brings up a 404 page
-    let isURLValid: Bool = {
-        let str = try? String(contentsOf: shareURL)
-        if let str = str {
-            return !str.contains("Sorry, the page you were looking for in this blog does not exist.")
-        } else {
-            return false
-        }
-    }()
-    
-    if isURLValid {
-        return shareURL
-    }
-    
-    return URL(string: GlobalLinks.blogURL)!
-}
-
-/**
- Gets the links within the `Post`
- 
- - returns: An array of `URL`s which are in the post.
- 
- - parameters:
-    - post: The selected post
- 
- - important: This process takes a bit so it is better to do it asyncronously so the app will not freeze while searching for URLs.
- 
- This method gets the URLs found within the blog post and filters out images from blogger's content delivery network because no one wants those URLs.
-*/
-func getLinksFromPost(post: Post) -> [URL] {
-    let items = post.content.components(separatedBy: "href=\"")
-    
-    var links: [URL] = []
-    
-    for item in items {
-        var newItem = ""
-        
-        for character in item {
-            if character != "\"" {
-                newItem += String(character)
-            } else {
-                break
-            }
-        }
-        
-        if let url = URL(string: newItem) {
-            links.append(url)
-        }
-    }
-    
-    links.removeDuplicates()
-    
-    links = links.filter { (link) -> Bool in
-        !link.absoluteString.contains("bp.blogspot.com/")
-    }
-    
-    return links
-}
-
-/**
  Launches post using the post title
  
  - parameters:
@@ -372,7 +272,6 @@ func launchPost(withTitle postTitle: String) {
     }
 
 }
-
 
 func continueFromCoreSpotlight(with userActivity: NSUserActivity) {
     if userActivity.activityType == CSSearchableItemActionType {
