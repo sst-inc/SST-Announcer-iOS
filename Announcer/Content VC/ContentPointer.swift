2c40d9e2602bd9f6733f9bc0c8fc2f21a2830c72
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-06-08 14:19:50 +0800

    cleaned up pointer

--- Announcer/Content VC/ContentPointer.swift
+++ Announcer/Content VC/ContentPointer.swift
@@ -9,13 +9,15 @@
 import Foundation
 import UIKit
 
+// For iPadOS 13.4 and up, pointer support
 @available(iOS 13.4, *)
 extension ContentViewController: UIPointerInteractionDelegate {
     func pointerInteraction(_ interaction: UIPointerInteraction, styleFor region: UIPointerRegion) -> UIPointerStyle? {
+        // If the view is a button, create interaction frame to be a little larger
         if interaction.view is UIButton {
-            
             let frame = CGRect(x: 0, y: 0, width: interaction.view!.frame.width + 5, height: interaction.view!.frame.height + 5)
             
+            // Round corners of selection
             return .init(shape: UIPointerShape.roundedRect(frame, radius: 30))
             
         } else if interaction.view is UICollectionViewCell {
@@ -31,11 +33,11 @@ extension ContentViewController: UIPointerInteractionDelegate {
         var pointerRegion: UIPointerRegion? = nil
         
         if let view = interaction.view as? UIButton {
+            // Handling scroll selection buttons
             pointerRegion = UIPointerRegion(rect: view.bounds,
                                             identifier: "pointer identifier")
-        }
-        
-        if let view = interaction.view as? UICollectionViewCell {
+        } else if let view = interaction.view as? UICollectionViewCell {
+            // Handling labels & links collection view cell
             pointerRegion = UIPointerRegion(rect: view.bounds,
                                             identifier: "selectedView")
         }
