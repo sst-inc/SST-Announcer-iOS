bacca123afe66ac55d5bf0ebe9ad3756c95b114f
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-07-25 00:32:45 +0800

    :dizzy: Hover Animations for Announcement TableView - New hover animations when user hovers over table view, on MacOS and iPadOS

--- Announcer/Content VC/CollectionView/LinksCollectionViewCell.swift
+++ Announcer/Content VC/CollectionView/LinksCollectionViewCell.swift
@@ -95,4 +95,31 @@ class LinksCollectionViewCell: UICollectionViewCell {
             }
         }
     }
+    
+    func loadView() {
+        let hover = UIHoverGestureRecognizer(target: self, action: #selector(hovered(sender:)))
+        
+        contentView.addGestureRecognizer(hover)
+    }
+    
+    @objc func hovered(sender: UIHoverGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            // When the hover begins
+            
+            
+            // Set the background color to show the highlight,
+            // mostly for dark mode users
+            backgroundColor = .systemGray4
+            
+        case .cancelled, .ended:
+            // When user stops hovering over feedback button
+            
+            // Resetting the background color, for dark mode users
+            backgroundColor = GlobalColors.greyTwo
+        default:
+            break
+        }
+
+    }
 }
