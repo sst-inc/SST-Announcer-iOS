b5ef340fdcf00774cb8438104f036aed32eb0f88
Jia Chen <36725840+jiaChenCloseParenthesesColon@users.noreply.github.com>
2020-08-29 00:04:25 +0800

    :bug: No double notifications - I completely forgot to remove the background tasks so it just continued refreshing although push notifications is fixed - yay

--- Announcer/Support Files/Delegates/AppDelegate.swift
+++ Announcer/Support Files/Delegates/AppDelegate.swift
@@ -51,29 +51,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // Notification delegate
         UNUserNotificationCenter.current().delegate = self
         
-        // Performing Background Fetch for new posts
-        BGTaskScheduler.shared.register(forTaskWithIdentifier: GlobalIdentifier.backgroundTask, using: .main) { task in
-            
-            // Check if there is a new post
-            if let notificationContent = Fetch.latestNotification() {
-                // Notify the user if there is a new post
-                self.pushNotification(with: notificationContent.title, content: notificationContent.content)
-            }
-            
-            task.setTaskCompleted(success: true)
-            
-            // Handle running out of time
-            task.expirationHandler = {
-                print("uh oh")
-            }
-            
-            // Schedule the next background task
-            self.scheduleBackgroundTaskIfNeeded()
-        }
-        
-        // Start background task
-        scheduleBackgroundTaskIfNeeded()
-        
         // Set project version
         if let versionNumber = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
             UserDefaults.standard.set(versionNumber, forKey: UserDefaultsIdentifiers.versionNumber.rawValue)
@@ -98,37 +75,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     }
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         self.pushNotifications.handleNotification(userInfo: userInfo)
-    }
-
-    // swiftlint:disable all
-    /**
-    Schedules the new announcement background task
-    
-     The identifier is `sg.edu.sst.panziyue.Announcer.new-announcement`
-     
-     ---
-    # Debugging Background Tasks
-     Running this line in the debugger to instantly manually background refresh.
-     Background refresh takes a while so this is the easier way to do it and test it out.
-     
-    [Developer Article](https://developer.apple.com/documentation/backgroundtasks/starting_and_terminating_tasks_during_development)
-     
-    ```
-     e -l objc -- (void)[[BGTaskScheduler sharedScheduler] _simulateLaunchForTaskWithIdentifier:@"sg.edu.sst.panziyue.Announcer.new-announcement"]
-     ```
-     */
-    // swiftlint:enable all
-    func scheduleBackgroundTaskIfNeeded() {
-        let taskRequest = BGProcessingTaskRequest(identifier: GlobalIdentifier.backgroundTask)
-        taskRequest.requiresNetworkConnectivity = true
-        
-        do {
-            try BGTaskScheduler.shared.submit(taskRequest)
-        } catch {
-            print("Unable to schedule background task: \(error)")
-        }
-        // To test if background tasks work, add a breakpoint here
-        // Then run the code above in the debugger
     }
     
     func application(_ app: UIApplication,
@@ -165,28 +111,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         // this will be called shortly after application:didFinishLaunchingWithOptions.
         //
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-    
-    // Push the notification to the user
-    func pushNotification(with postTitle: String, content postContent: String) {
-        let content = UNMutableNotificationContent()
-        
-        content.title = "📢 \(postTitle)"
-        content.body = postContent
-        
-        content.sound = .default
-        
-        let request = UNNotificationRequest(identifier: GlobalIdentifier.newNotification,
-                                            content: content,
-                                            trigger: nil)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            DispatchQueue.main.async {
-                if let error = error {
-                    print("Error showing error user notification: \(error)")
-                }
-            }
-        }
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter,
